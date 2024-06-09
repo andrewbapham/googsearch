e40181022a88cb08dff9fd827bf184438e40ad43
Andrew Pham <andrewbapham@outlook.com>
2024-06-09 19:29:03 -0400

    Add alias replacement and domain validation

--- src/googsearch.go
+++ src/googsearch.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 //	"net/http"
+  //"net/url"
 	"os"
-//	"strings"
+  "strings"
+  "regexp"
 //	"time"
-
+  
 	"github.com/urfave/cli/v2"
 //	"golang.org/x/net/html"
 )
@@ -45,5 +47,34 @@ func main() {
 func main_func(*cli.Context) error{
   fmt.Println("Flag website: ", websites)
   fmt.Println("Flag number:", show_result_count)
+  website_filter_list := get_website_list_from_string(websites)
+  fmt.Println("Filter list: ", website_filter_list)
   return nil
+}
+
+func get_website_list_from_string(str string) []string{
+  entries := strings.Split(strings.ToLower(str), " ")
+
+  var alias_map = map[string]string{
+    "so": "stackoverflow.com",
+    "gh": "github.com",
+  }
+
+  for i, w := range entries{
+    url, ok := alias_map[w]
+    if ok {
+      entries[i] = url
+    }else if !is_valid_domain(w) {
+      log.Fatalf("Input filter %s is not a valid alias or is an invalid domain", w)
+    }
+  }
+
+  return entries
+}
+
+func is_valid_domain(str string) bool{
+  pattern, _ := regexp.Compile(`^([A-Za-z0-9-]{1, 63}\\.)+[A-Za-z]{2, 6}$`)
+  starts_with_dash, _ := regexp.Compile(`-.*`)
+  domain_ends_with_dash, _ := regexp.Compile(`^.*-\.`)
+  return pattern.MatchString(str) && !starts_with_dash.MatchString(str) && ! domain_ends_with_dash.MatchString(str)
 }
